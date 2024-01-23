c2b58573493c4dfbee20b9e57e42dd606ff4e0e2
Byron Realpe <byron.realpe@kfc.com.ec>
2024-01-23 11:27:49 -0500

    Estos cambios son test

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("C:\\github\\documentacion-DSI\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\github\\documentacion-DSI\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\github\\documentacion-DSI\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\github\\documentacion-DSI\\src\\css\\custom.css"),
+  require("C:\\github\\test\\documentacion-DSI\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\github\\test\\documentacion-DSI\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\github\\test\\documentacion-DSI\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\github\\test\\documentacion-DSI\\src\\css\\custom.css"),
 ];
--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -4,111 +4,123 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 export default [
   {
     path: '/documentacion-DSI/__docusaurus/debug',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug', '8db'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug', '042'),
     exact: true
   },
   {
     path: '/documentacion-DSI/__docusaurus/debug/config',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/config', '754'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/config', 'e37'),
     exact: true
   },
   {
     path: '/documentacion-DSI/__docusaurus/debug/content',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/content', 'a6b'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/content', '335'),
     exact: true
   },
   {
     path: '/documentacion-DSI/__docusaurus/debug/globalData',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/globalData', '034'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/globalData', '6a4'),
     exact: true
   },
   {
     path: '/documentacion-DSI/__docusaurus/debug/metadata',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/metadata', 'f50'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/metadata', 'f24'),
     exact: true
   },
   {
     path: '/documentacion-DSI/__docusaurus/debug/registry',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/registry', 'bf7'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/registry', 'f3a'),
     exact: true
   },
   {
     path: '/documentacion-DSI/__docusaurus/debug/routes',
-    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/routes', 'c1d'),
+    component: ComponentCreator('/documentacion-DSI/__docusaurus/debug/routes', 'f84'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog',
-    component: ComponentCreator('/documentacion-DSI/blog', '80f'),
+    component: ComponentCreator('/documentacion-DSI/blog', '221'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/archive',
-    component: ComponentCreator('/documentacion-DSI/blog/archive', 'f1d'),
+    component: ComponentCreator('/documentacion-DSI/blog/archive', '10f'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/first-blog-post',
-    component: ComponentCreator('/documentacion-DSI/blog/first-blog-post', 'd60'),
+    component: ComponentCreator('/documentacion-DSI/blog/first-blog-post', 'e05'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/long-blog-post',
-    component: ComponentCreator('/documentacion-DSI/blog/long-blog-post', '802'),
+    component: ComponentCreator('/documentacion-DSI/blog/long-blog-post', 'eb4'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/mdx-blog-post',
-    component: ComponentCreator('/documentacion-DSI/blog/mdx-blog-post', 'b4b'),
+    component: ComponentCreator('/documentacion-DSI/blog/mdx-blog-post', '6c6'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/tags',
-    component: ComponentCreator('/documentacion-DSI/blog/tags', 'c29'),
+    component: ComponentCreator('/documentacion-DSI/blog/tags', '984'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/tags/docusaurus',
-    component: ComponentCreator('/documentacion-DSI/blog/tags/docusaurus', 'b8c'),
+    component: ComponentCreator('/documentacion-DSI/blog/tags/docusaurus', '8b8'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/tags/facebook',
-    component: ComponentCreator('/documentacion-DSI/blog/tags/facebook', 'fb8'),
+    component: ComponentCreator('/documentacion-DSI/blog/tags/facebook', '73a'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/tags/hello',
-    component: ComponentCreator('/documentacion-DSI/blog/tags/hello', '86e'),
+    component: ComponentCreator('/documentacion-DSI/blog/tags/hello', '815'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/tags/hola',
-    component: ComponentCreator('/documentacion-DSI/blog/tags/hola', 'd94'),
+    component: ComponentCreator('/documentacion-DSI/blog/tags/hola', 'c15'),
     exact: true
   },
   {
     path: '/documentacion-DSI/blog/welcome',
-    component: ComponentCreator('/documentacion-DSI/blog/welcome', 'd6b'),
+    component: ComponentCreator('/documentacion-DSI/blog/welcome', 'b46'),
     exact: true
   },
   {
     path: '/documentacion-DSI/markdown-page',
-    component: ComponentCreator('/documentacion-DSI/markdown-page', '2d3'),
+    component: ComponentCreator('/documentacion-DSI/markdown-page', '421'),
     exact: true
   },
   {
     path: '/documentacion-DSI/docs',
-    component: ComponentCreator('/documentacion-DSI/docs', '77a'),
+    component: ComponentCreator('/documentacion-DSI/docs', '685'),
     routes: [
       {
         path: '/documentacion-DSI/docs',
-        component: ComponentCreator('/documentacion-DSI/docs', '160'),
+        component: ComponentCreator('/documentacion-DSI/docs', 'a9a'),
         routes: [
           {
             path: '/documentacion-DSI/docs',
-            component: ComponentCreator('/documentacion-DSI/docs', '502'),
+            component: ComponentCreator('/documentacion-DSI/docs', '443'),
             routes: [
+              {
+                path: '/documentacion-DSI/docs/category/facturación-electronica',
+                component: ComponentCreator('/documentacion-DSI/docs/category/facturación-electronica', '0f8'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/category/facturación-electronica-1',
+                component: ComponentCreator('/documentacion-DSI/docs/category/facturación-electronica-1', '963'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
               {
                 path: '/documentacion-DSI/docs/category/maxpoint',
                 component: ComponentCreator('/documentacion-DSI/docs/category/maxpoint', 'ecb'),
@@ -124,6 +136,60 @@ export default [
               {
                 path: '/documentacion-DSI/docs/category/tutorial---extras',
                 component: ComponentCreator('/documentacion-DSI/docs/category/tutorial---extras', '64c'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/congratulations',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/congratulations', '81a'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/create-a-blog-post',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/create-a-blog-post', '205'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/create-a-document',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/create-a-document', '942'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/create-a-page',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/create-a-page', '48c'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/deploy-your-site',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/deploy-your-site', 'b6a'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/MANUAL DEL SISTEMA  MAXPOINT',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/MANUAL DEL SISTEMA  MAXPOINT', '945'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/markdown-features',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/markdown-features', '9cb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/Maxpoint',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Facturacion Electronica copy/Maxpoint', '67d'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/documentacion-DSI/docs/Facturacion Electronica/Maxpoint',
+                component: ComponentCreator('/documentacion-DSI/docs/Facturacion Electronica/Maxpoint', 'f0f'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
@@ -237,7 +303,7 @@ export default [
   },
   {
     path: '/documentacion-DSI/',
-    component: ComponentCreator('/documentacion-DSI/', 'c2c'),
+    component: ComponentCreator('/documentacion-DSI/', '470'),
     exact: true
   },
   {
