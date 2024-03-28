675d3072c2922bb942ad69da4dc7ca36c939a6e9
Byron Antonio <121568777+ByronAnto@users.noreply.github.com>
2024-03-28 11:04:18 -0500

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\Adri\\Escritorio\\Project\\documentacion-DSI\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\Adri\\Escritorio\\Project\\documentacion-DSI\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\Adri\\Escritorio\\Project\\documentacion-DSI\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\Adri\\Escritorio\\Project\\documentacion-DSI\\src\\css\\custom.css"),
-];
